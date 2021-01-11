b88e5b3deb804dfeda3d551c3c5bb25c9cf0c2b7
Leonardo Dutra <dupeleonardo@gmail.com>
2021-01-11 15:37:14 -0300

    complete calculator.js:     -fix sequential operations issue     -division by 0

--- calculator.js
+++ calculator.js
@@ -1,14 +1,32 @@
 const calculatorKeys = document.querySelectorAll(".calculator-btn");
 const calculatorDisplay = document.querySelector(".calculator-display");
 
+let lastOperator = ""
+    , nextOperator = false;
+
+let oldValue
+    , newValue = 0
+    , newValueInput = false
+    , result = 0;
+
+resetDisplay();
 calculatorKeys.forEach(function(key) {
     key.addEventListener("click", function() {
         if (isNumber(key.value)) {
-            calculatorDisplay.textContent += key.value;
+            calculatorDisplay.valueAsNumber += key.value;
+            if (lastOperator) {
+                if (nextOperator && newValueInput === false) {
+                    newValue = 0;
+                    newValueInput = true;
+                } 
+                newValue += key.value;
+                newValue = Number(newValue);
+                calculatorDisplay.valueAsNumber = newValue;
+            }
         } else {
-            checkOperator(key.value);
+            calculate(key.value, calculatorDisplay.valueAsNumber);
+            newValueInput = false;
         }
-        
     });
 });
 
@@ -20,22 +38,91 @@ function isNumber(keyPressed) {
     }
 }
 
-function checkOperator(operator) {
-    if (operator === "C") {
-        clearDisplay();
-    } else {
-        calculate();
+function calculate(operator, displayValue) {
+    switch (operator) {
+        case "C":
+            resetDisplay();
+            resetOperators();
+            resetValues();
+            break;
+        case "+-":
+            calculatorDisplay.valueAsNumber = opposite(displayValue);
+            break;
+        case "%":
+            calculatorDisplay.valueAsNumber = percentage(displayValue);
+            break;            
+        case "=":
+            equal(displayValue);
+            break;
+        default:
+            lastOperator = operator;
+            oldValue = displayValue;
+            result = displayValue;
     }
 }
 
-function calculate() {
-    const 
+function resetDisplay() {
+    calculatorDisplay.valueAsNumber = 0;
+    calculatorDisplay.placeholder = "";
+}
+
+function resetOperators() {
+    lastOperator = "";
+    nextOperator = false;
+}
+
+function resetValues() {
+    result = 0;
+    oldValue = 0;
+    newValue = 0;
+    newValueInput = false;
+}
+
+function opposite(displayValue) {
+    return -displayValue;
+}
+
+function percentage(displayValue) {
+    return displayValue / 100;
+}
+
+function divide() {
+    result /= newValue; 
+    if (result === Infinity)
+        calculatorDisplay.placeholder = "Infinity";
+    else
+        return result;
+}
+
+function multiply() {
+    result *= newValue;
+    return result;
+}
+
+function subtract() {
+    result -= newValue;
+    return result;
 }
 
 function add() {
-
+    result += newValue;
+    return result;
 }
 
-function clearDisplay() {
-    calculatorDisplay.textContent = "";
+function equal() {
+    switch (lastOperator) {
+        case "/":
+            calculatorDisplay.valueAsNumber = divide();
+            break;
+        case "*":
+            calculatorDisplay.valueAsNumber = multiply();
+            break;
+        case "-":
+            calculatorDisplay.valueAsNumber = subtract();
+            break;
+        case "+":
+            calculatorDisplay.valueAsNumber = add();
+            break;
+    }
+    nextOperator = true;
 }
